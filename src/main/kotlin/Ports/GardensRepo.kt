e2a6d60a8e0adeb8453a6f4b2820f1373108f55a
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-17 12:10:34 +0100

    Feat: addition of function to GardensRepo interface

--- src/main/kotlin/Ports/GardensRepo.kt
+++ src/main/kotlin/Ports/GardensRepo.kt
@@ -3,5 +3,7 @@ package com.example.Ports
 import com.example.domain.models.Garden
 
 interface GardensRepo {
-    fun getGardens(): List<Garden>
+    fun getAllGardens(): List<Garden>
+
+    fun add()
 }
