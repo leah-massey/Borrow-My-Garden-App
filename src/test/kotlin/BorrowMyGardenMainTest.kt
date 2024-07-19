4ce8b8c3a02112118805f8aa21ead92c683adb35
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-19 14:28:28 +0100

    Test: connected test to db

--- src/test/kotlin/BorrowMyGardenMainTest.kt
+++ src/test/kotlin/BorrowMyGardenMainTest.kt
@@ -1,17 +1,27 @@
 package com.example
 
+import com.example.Adapters.GardensPostgresRepo
+import com.example.Adapters.HttpAPI
+import com.example.Ports.GardensRepo
+import com.example.domain.ReadDomain
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.postgresql.ds.PGSimpleDataSource
 
 class BorrowMyGardenMainTest {
-
     @Test
     fun `Ping test`() {
-        assertEquals(Response(OK).body("pong"), app(Request(GET, "/ping")))
-    }
+        val gardensRepoDatasource: GardensRepo = GardensPostgresRepo(PGSimpleDataSource().apply {
+            user = "postgres"
+            databaseName = "borrowmygarden"
+        })
+        val readDomain = ReadDomain(gardensRepoDatasource)
+        val httpAPI = HttpAPI(readDomain)
 
+        assertEquals(Response(OK).body("pong"), httpAPI.app(Request(GET, "/ping")))
+    }
 }
