eb774cae7312f27d9c729f951c56fe6e27f276f8
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-19 14:21:26 +0100

    Feat: all gardens are retrieved from the database

--- src/main/kotlin/Adapters/GardensPostgresRepo.kt
+++ src/main/kotlin/Adapters/GardensPostgresRepo.kt
@@ -2,6 +2,7 @@ package com.example.Adapters
 
 import com.example.Ports.GardensRepo
 import com.example.database.GardensTable
+import com.example.database.all
 import com.example.domain.models.Garden
 import org.http4k.sse.SseMessage
 import org.jetbrains.exposed.sql.Database
@@ -13,35 +14,12 @@ import org.postgresql.ds.PGSimpleDataSource
 class GardensPostgresRepo(datasource: PGSimpleDataSource): GardensRepo {
     val database = Database.connect(datasource)
     override fun getAllGardens(): List<Garden> {
-        return GardensTable.all()
+        return transaction(database) {
+            GardensTable.all()
+        }
     }
     override fun add() {
         TODO("Not yet implemented")
     }
-
-
 }
 
-// not sure where I should keep these?? 👇🏻
-fun GardensTable.all() = selectAll().map {
-    it.toGarden()
-}
-
-fun ResultRow.toGarden(): Garden {
-    val id = this[GardensTable.id]
-    val createdTimestamp = this[GardensTable.created_timestamp]
-    val title = this[GardensTable.title]
-    val description = this[GardensTable.description]
-    val gardenOwnerFirstName = this[GardensTable.garden_owner_first_name]
-    val gardenOwnerId = this[GardensTable.garden_owner_id]
-    val gardenStatus = this[GardensTable.garden_status]
-    return Garden(
-        id,
-        createdTimestamp = createdTimestamp,
-        title,
-        description,
-        gardenOwnerFirstName = gardenOwnerFirstName,
-        gardenOwnerId = gardenOwnerId,
-        gardenStatus = gardenStatus
-    )
-}
