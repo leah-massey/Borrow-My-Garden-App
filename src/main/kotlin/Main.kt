05d2c9ba6c6f2eedc928bb3f9e53d7019ef70668
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-19 12:39:02 +0100

    Chore: database added to main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,27 @@
 package com.example
 
 import com.example.Adapters.GardensPostgresRepo
+import com.example.Adapters.HttpAPI
 import com.example.Ports.GardensRepo
-import com.example.formats.JacksonMessage
-import com.example.formats.jacksonMessageLens
+import com.example.domain.ReadDomain
 import org.http4k.core.HttpHandler
-import org.http4k.core.Method.GET
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
-import org.http4k.core.with
 import org.http4k.filter.DebuggingFilters.PrintRequest
-import org.http4k.routing.bind
-import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import org.postgresql.ds.PGSimpleDataSource
 
-val app: HttpHandler = routes(
-    "/ping" bind GET to {
-        Response(OK).body("pong")
-    },
 
-    "/formats/json/jackson" bind GET to {
-        Response(OK).with(jacksonMessageLens of JacksonMessage("Barry", "Hello there!"))
-    }
-)
 
 fun main() {
-    val gardensRepo: GardensRepo = GardensPostgresRepo(PGSimpleDataSource().apply {
+    val gardensRepoDatasource: GardensRepo = GardensPostgresRepo(PGSimpleDataSource().apply {
         user = "postgres"
         databaseName = "borrowmygarden"
     })
-    val printingApp: HttpHandler = PrintRequest().then(app)
+    val readDomain = ReadDomain(gardensRepoDatasource)
+    val httpAPI = HttpAPI(readDomain)
+
+    val printingApp: HttpHandler = PrintRequest().then(httpAPI.app)
 
     val server = printingApp.asServer(SunHttp(9000)).start()
 
