0472d3f69d1bb586c2a8545dbf53d477a80fe266
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-19 12:44:30 +0100

    Feat: endpoint added that returns all gardens from db in json format

---
+++ src/main/kotlin/Adapters/HttpAPI.kt
@@ -0,0 +1,34 @@
+package com.example.Adapters
+
+import com.example.domain.ReadDomain
+import com.example.domain.models.Garden
+import com.example.formats.JacksonMessage
+import com.example.formats.jacksonMessageLens
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.http4k.core.*
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+class HttpAPI(readDomain: ReadDomain) {
+
+    val app: HttpHandler = routes(
+
+        "/gardens" bind Method.GET to { request: Request ->
+            val gardens: List<Garden> = readDomain.viewGardens()
+            val gardensAsJsonString: String = mapper.writeValueAsString(gardens)
+
+            Response(Status.OK)
+                .body(gardensAsJsonString)
+                .header("content-type", "application/json")
+        },
+
+
+        "/ping" bind Method.GET to {
+            Response(Status.OK).body("pong")
+        }
+
+
+    )
+    private val mapper: ObjectMapper = jacksonObjectMapper()
+}
