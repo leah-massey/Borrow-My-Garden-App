217813f723c5ec481427fe29f643bc8700d1be86
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-19 12:45:08 +0100

    Refactor: functions relocated for clarity

--- src/main/kotlin/database/GardensTable.kt
+++ src/main/kotlin/database/GardensTable.kt
@@ -1,8 +1,9 @@
 package com.example.database
 
+import com.example.database.insert
+import com.example.domain.models.Garden
 import com.example.domain.models.GardenStatus
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
 import java.util.*
 
 
@@ -16,3 +17,38 @@ import java.util.*
         val garden_status: Column<GardenStatus> = enumerationByName("gardenStatus", length = 100)
         override val primaryKey = PrimaryKey(id, name = "PK_Gardens_Id")
     }
+
+fun GardensTable.all() = selectAll().map {
+    it.toGarden()
+}
+
+fun ResultRow.toGarden(): Garden {
+    val id = this[GardensTable.id]
+    val createdTimestamp = this[GardensTable.created_timestamp]
+    val title = this[GardensTable.title]
+    val description = this[GardensTable.description]
+    val gardenOwnerFirstName = this[GardensTable.garden_owner_first_name]
+    val gardenOwnerId = this[GardensTable.garden_owner_id]
+    val gardenStatus = this[GardensTable.garden_status]
+    return Garden(
+        id,
+        createdTimestamp = createdTimestamp,
+        title,
+        description,
+        gardenOwnerFirstName = gardenOwnerFirstName,
+        gardenOwnerId = gardenOwnerId,
+        gardenStatus = gardenStatus
+    )
+}
+
+fun GardensTable.insert(garden: Garden) {
+    insert {
+        it[id] = garden.id
+        it[created_timestamp] = garden.createdTimestamp
+        it[title] = garden.title
+        it[description] = garden.description
+        it[garden_owner_first_name] = garden.gardenOwnerFirstName
+        it[garden_owner_id] = garden.gardenOwnerId
+        it[garden_status] = garden.gardenStatus
+    }
+}
