84f0c240ea8c0613ddc1fc36bc8273493c2b46df
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-07-23 16:11:19 +0100

    Chore: project set up - steel thread established between DB, BE and FE

--- src/main/kotlin/Adapters/HttpAPI.kt
+++ src/main/kotlin/Adapters/HttpAPI.kt
@@ -2,17 +2,14 @@ package com.example.Adapters
 
 import com.example.domain.ReadDomain
 import com.example.domain.models.Garden
-import com.example.formats.JacksonMessage
-import com.example.formats.jacksonMessageLens
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.http4k.core.*
+import org.http4k.format.Jackson.mapper
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 
 class HttpAPI(readDomain: ReadDomain) {
 
-    val app: HttpHandler = routes(
+val app: HttpHandler = routes(
 
         "/api/gardens" bind Method.GET to { request: Request ->
             val gardens: List<Garden> = readDomain.viewGardens()
@@ -21,14 +18,11 @@ class HttpAPI(readDomain: ReadDomain) {
             Response(Status.OK)
                 .body(gardensAsJsonString)
                 .header("content-type", "application/json")
+                .header("Access-Control-Allow-Origin", "http://localhost:3000")
         },
-
 
         "/ping" bind Method.GET to {
             Response(Status.OK).body("pong")
         }
-
-
     )
-    private val mapper: ObjectMapper = jacksonObjectMapper()
 }
