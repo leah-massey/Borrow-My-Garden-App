c0b4fdfa86bcfd774c219eb3e2d2ea2a4c82966f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-08-19 15:14:00 +0100

    Refactor: updated to use Mockk library

--- src/test/kotlin/domain/ReadDomainTest.kt
+++ src/test/kotlin/domain/ReadDomainTest.kt
@@ -4,11 +4,13 @@ import com.example.Ports.GardensRepo
 import com.example.domain.ReadDomain
 import com.example.domain.models.Garden
 import com.example.domain.models.GardenStatus
+import io.mockk.every
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.*
+import io.mockk.mockk
 
 
 class ReadDomainTest {
@@ -17,13 +19,13 @@ class ReadDomainTest {
     val testGarden2 = Garden(id = UUID.randomUUID(), createdTimestamp = "20230723T083000Z", title = "Shady Retreat", description = "A peaceful garden with lots of shade", gardenStatus = GardenStatus.AVAILABLE, gardenOwnerFirstName = "Alice", gardenOwnerId = UUID.randomUUID())
     val testGarden3 = Garden(id = UUID.randomUUID(), createdTimestamp = "456", title = "Oasis in the city", description = "This is the place that you can come to to relax", gardenStatus = GardenStatus.NOT_AVAILABLE, gardenOwnerFirstName = "Jim", gardenOwnerId = UUID.randomUUID())
     val testGarden4 = Garden(id = UUID.randomUUID(), createdTimestamp = "147", title = "Blank Canvas", description = "You can do what you like with this garden - lots of nettles and rubble but could be great", gardenStatus = GardenStatus.NOT_AVAILABLE, gardenOwnerFirstName = "Buzz", gardenOwnerId = UUID.randomUUID())
-    val mockGardenRepo: GardensRepo = mock(GardensRepo::class.java)
+    val mockGardenRepo: GardensRepo = mockk<GardensRepo>()
 
     @Test
     fun `given a single available garden is in the repo, when the user requests to view the gardens, the garden is returned`() {
         //given
         val testGardens: List<Garden> = listOf(testGarden1)
-        `when` (mockGardenRepo.getAllGardens()).thenReturn(testGardens)
+        every { mockGardenRepo.getAllGardens() } returns testGardens
         val underTest = ReadDomain(mockGardenRepo)
 
         // when
@@ -38,7 +40,7 @@ class ReadDomainTest {
     fun `given multiple gardens are in the repo, with varying availability, when the user requests to view the gardens, only the available gardens are returned`() {
         //given
         val testGardens: List<Garden> = listOf(testGarden1, testGarden2, testGarden3, testGarden4)
-        `when` (mockGardenRepo.getAllGardens()).thenReturn(testGardens)
+        every { mockGardenRepo.getAllGardens() } returns testGardens
         val underTest = ReadDomain(mockGardenRepo)
 
         // when
@@ -53,8 +55,7 @@ class ReadDomainTest {
     @Test
     fun `a single garden is returned`() {
         // given
-        val testGardens: List<Garden> = listOf(testGarden1, testGarden2, testGarden3, testGarden4)
-        `when` (mockGardenRepo.getGarden(testGarden3.id)).thenReturn(testGarden3)
+        every {mockGardenRepo.getGarden(testGarden3.id)} returns testGarden3
         val underTest = ReadDomain(mockGardenRepo)
 
         // when
