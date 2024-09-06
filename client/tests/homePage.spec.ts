799b5197a9f5999211bb1cf4a5d703aa925369dd
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-06 22:32:14 +0100

    test: e2e test home page links to individual garden page - currently failing

--- client/tests/homePage.spec.ts
+++ client/tests/homePage.spec.ts
@@ -5,3 +5,11 @@ test('has title', async({ page}) => {
 
     await expect(page).toHaveTitle(/Gardens/)
 })
+
+test('individual garden link', async ({ page}) => {
+    await page.goto('http://localhost:5173/');
+
+    await page.getByRole('link', {name: 'Garden with good soil'}).click();
+
+    await expect(page.getByRole('heading', {name: 'Garden with good soil'})).toBeVisible();
+})
