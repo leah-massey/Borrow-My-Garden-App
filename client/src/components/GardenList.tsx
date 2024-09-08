18ac2775fe5f929b994c47d90889b57fb5058811
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-08 13:36:05 +0100

    refactor: remove excess spacing

--- client/src/components/GardenList.tsx
+++ client/src/components/GardenList.tsx
@@ -4,18 +4,13 @@ import {Link} from "react-router-dom";
 const GardenList = (props) => {
     const gardens = props.gardens
 
-    console.log(props, gardens)
-
     return (
         <div className="garden-list">
             {gardens.map(garden => (
                 <div key = {garden.id}>
-
-                    <Link to={garden.id} id={garden.id}>
+                    <Link to={garden.id}>
                         {garden.title}
                     </Link>
-
-
                 </div>
             ))}
         </div>
