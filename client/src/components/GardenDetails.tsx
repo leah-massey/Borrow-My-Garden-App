7326d0558b7eada58a7d1336a1beb6339a759b7f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-09 15:38:54 +0100

    feat: title and description of single garden is accessible via link from Gardens page

--- client/src/components/GardenDetails.tsx
+++ client/src/components/GardenDetails.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react'
+import {Link, useParams} from "react-router-dom";
+import {Garden} from "../pages/gardens/GardensPage.tsx";
 
 const GardenDetails = () => {
+    const {gardenId} = useParams()
+    console.log(`this is the garden id: ${gardenId}`)
+    const [gardenDetails, setGardenDetails] = useState<Garden[]> (null)
 
+    useEffect( () => {
+        const fetchGardenDetails = async () => {
+            try {
+                const response = await fetch('http://localhost:9000/internal/gardens/' + gardenId);
+                const body: Garden[] = await response.json();
+                setGardenDetails(body);
+            }
+            catch (error: unknown) {
+                if (error instanceof Error) {
+                    console.error(`error fetching garden ${gardenId}`, error.message)
+                } else {
+                    console.error(`Error fetching garden ${gardenId}:`, error)
+                }
+            }
+        }
+        fetchGardenDetails()
+    }, []);
 
     return (
-        <div className="blog-details">
-            <h2>Garden Details</h2>
-        </div>
+            <div className="blog-details">
+                {gardenDetails.map(garden => (
+                    <div>
+                        <h3>{garden.title}</h3>
+                        <p>{garden.description}</p>
+                    </div>
+                ))}
+            </div>
+
     )
 }
 
