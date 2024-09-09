d5ba18b3d416dfbb6db2950da4f5ef764dbbd39e
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-09 17:29:17 +0100

    refactor: comment out unused tests

--- src/test/kotlin/PostGresTests/PostgresTestsUser.kt
+++ src/test/kotlin/PostGresTests/PostgresTestsUser.kt
@@ -14,56 +14,58 @@ import org.postgresql.ds.PGSimpleDataSource
 import java.util.*
 import kotlin.test.assertEquals
 
-
-class PostGresTestsUser {
-
-    val dataSource = PGSimpleDataSource().apply {
-        user = "postgres"
-        databaseName = "borrowmygarden"
-    }
-
-    val database = Database.connect(dataSource)
-
-    @BeforeEach fun resetDB() {
-        transaction(database) {
-            SchemaUtils.drop(Users)
-            SchemaUtils.createMissingTablesAndColumns(Users) // you can add multiple tables here within the brackets
-        }
-    }
-    @Test
-    fun `a user can be added to and retrieved from the database`() {
-        val user = User(UUID.randomUUID(), "Jenny", "Long", MembershipStatus.ACTIVE )
-        transaction(database) {
-            Users.insert(user)
-            val readUsers: List<User> = Users.all()
-            assertEquals(readUsers, listOf(user))
-        }
-    }
-}
-private fun Users.insert(user: User) {
-    insert {
-        it[id] = user.id
-        it[firstName] = user.firstName
-        it[secondName] = user.secondName
-        it[membershipStatus] = user.membershipStatus
-    }
-}
-private fun Users.all(): List<User> {
-    val readUsers: List<User> = selectAll().map {
-        val id = it[id]
-        val firstName = it[firstName]
-        val secondName = it[secondName]
-        val membershipStatus = it[membershipStatus]
-        User(id, firstName, secondName, membershipStatus)
-    }
-    return readUsers
-}
+//code for this yet to be written
 
 
-object Users: Table() {
-    val id: Column<UUID> = uuid("id")
-    val firstName: Column<String> = varchar("firstName", 100)
-    val secondName: Column<String> = varchar("secondName", 100)
-    val membershipStatus: Column<MembershipStatus> = enumerationByName("membershipStatus", 20, MembershipStatus::class)
-    override val primaryKey = PrimaryKey(id, name = "PK_Users_Id")
-}
+//class PostGresTestsUser {
+//
+//    val dataSource = PGSimpleDataSource().apply {
+//        user = "postgres"
+//        databaseName = "borrowmygarden"
+//    }
+//
+//    val database = Database.connect(dataSource)
+//
+//    @BeforeEach fun resetDB() {
+//        transaction(database) {
+//            SchemaUtils.drop(Users)
+//            SchemaUtils.createMissingTablesAndColumns(Users) // you can add multiple tables here within the brackets
+//        }
+//    }
+//    @Test
+//    fun `a user can be added to and retrieved from the database`() {
+//        val user = User(UUID.randomUUID(), "Jenny", "Long", MembershipStatus.ACTIVE )
+//        transaction(database) {
+//            Users.insert(user)
+//            val readUsers: List<User> = Users.all()
+//            assertEquals(readUsers, listOf(user))
+//        }
+//    }
+//}
+//private fun Users.insert(user: User) {
+//    insert {
+//        it[id] = user.id
+//        it[firstName] = user.firstName
+//        it[secondName] = user.secondName
+//        it[membershipStatus] = user.membershipStatus
+//    }
+//}
+//private fun Users.all(): List<User> {
+//    val readUsers: List<User> = selectAll().map {
+//        val id = it[id]
+//        val firstName = it[firstName]
+//        val secondName = it[secondName]
+//        val membershipStatus = it[membershipStatus]
+//        User(id, firstName, secondName, membershipStatus)
+//    }
+//    return readUsers
+//}
+//
+//
+//object Users: Table() {
+//    val id: Column<UUID> = uuid("id")
+//    val firstName: Column<String> = varchar("firstName", 100)
+//    val secondName: Column<String> = varchar("secondName", 100)
+//    val membershipStatus: Column<MembershipStatus> = enumerationByName("membershipStatus", 20, MembershipStatus::class)
+//    override val primaryKey = PrimaryKey(id, name = "PK_Users_Id")
+//}
