dd044081ac4f71d32d4aaf53f4a0973313c5ecc5
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-18 13:46:10 +0100

    test: the delete method is triggered in the GardensRepo

--- src/main/kotlin/Ports/WriteDomain.kt
+++ src/main/kotlin/Ports/WriteDomain.kt
@@ -1,9 +1,11 @@
 package com.example.Ports
 
 import com.example.domain.models.Garden
+import java.util.*
 
 interface WriteDomain {
 
     //should I have it return something??
     fun addGarden(garden: Garden)
+    fun deleteGarden(gardenId: UUID)
 }
--- src/test/kotlin/domain/WriteDomainTest.kt
+++ src/test/kotlin/domain/WriteDomainTest.kt
@@ -9,7 +9,7 @@ import java.util.*
 
 class WriteDomainTest {
     @Test
-    fun `adds a garden to the db`() {
+    fun `the add garden method is triggered in the GardensRepo`() {
         //is there a better way of testing this?
 
         // given
@@ -24,11 +24,29 @@ class WriteDomainTest {
 
         val mockGardenRepo: GardensRepo = Mockito.mock(GardensRepo::class.java)
         val writeDomain = WriteDomain(mockGardenRepo)
-
         // when
         writeDomain.addGarden(newGarden)
-
         //then
-       Mockito.verify(mockGardenRepo).add(newGarden)
+       Mockito.verify(mockGardenRepo, Mockito.times(1)).add(newGarden)
+    }
+
+    @Test
+    fun `the delete method is triggered in the GardensRepo`() {
+        // given
+        val mockGardenRepo: GardensRepo = Mockito.mock(GardensRepo::class.java)
+        val writeDomain = WriteDomain(mockGardenRepo)
+
+        val gardenToBeDeleted = Garden(
+            id = UUID.fromString("de5ef0e7-0dbe-479c-a7c8-9f12db7ce225"),
+            createdTimestamp = "123",
+            title = "Sunny Garden",
+            description = "very sunny garden",
+            gardenOwnerFirstName = "Bev",
+            gardenOwnerId = UUID.fromString("73b0210b-3f30-4764-a31d-e25a83840eb7")
+        )
+        // when
+        writeDomain.deleteGarden(gardenToBeDeleted.id)
+        // then
+        Mockito.verify(mockGardenRepo, Mockito.times(1)).delete(gardenToBeDeleted.id)
     }
 }
