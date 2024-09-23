c7103143dc56dde27e2088ecc2c182cf540cb499
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-09-23 11:51:23 +0100

    chore: Chakra installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import GardensPage from "./pages/gardens/GardensPage.tsx";
 import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
@@ -6,25 +5,26 @@ import Layout from "./components/Layout.tsx";
 import HomePage from "./pages/homePage/HomePage.tsx";
 import GardenDetails from "./components/GardenDetails.tsx";
 import AddGardenForm from "./components/AddGardenForm.tsx";
-
-
+import {ChakraProvider} from '@chakra-ui/react'
 
 
 const App = () => {
 
     return (
-<div className="bg-lime-100 min-h-screen w-full">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<HomePage />}/>
-                    <Route path={"gardens"} element={<GardensPage />}/>
-                    <Route path={"gardens/:gardenId"} element={<GardenDetails />} />
-                    <Route path={"add-garden"} element={<AddGardenForm />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-</div>
+        <ChakraProvider>
+            <div className="bg-lime-100 min-h-screen w-full">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout/>}>
+                            <Route index element={<HomePage/>}/>
+                            <Route path={"gardens"} element={<GardensPage/>}/>
+                            <Route path={"gardens/:gardenId"} element={<GardenDetails/>}/>
+                            <Route path={"add-garden"} element={<AddGardenForm/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </ChakraProvider>
 
     )
 
