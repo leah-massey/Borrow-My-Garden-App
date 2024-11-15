1b183b6f375d1069c622f3a71cedf0d420779fe6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-11-15 09:31:30 +0000

    Refactor: port number updated for container DB, DB Host and Port added

--- scripts/dockerDatabaseBuild.sh
+++ scripts/dockerDatabaseBuild.sh
@@ -2,5 +2,5 @@
 docker pull postgres
 
 # create database container
-docker run --name borrow-my-garden-db --network borrow-my-garden-network -p 5432:5432 -v borrow-my-garden-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --name borrow-my-garden-db --network borrow-my-garden-network -p 6543:5432 -v borrow-my-garden-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 
