e0ebefb564af1babff63602c6cf9c743f3551f94
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-11-16 17:16:29 +0000

    Refactor: docker containers are now set up using docker compose

--- scripts/dockerDatabaseBuild.sh
+++
@@ -1,6 +0,0 @@
-# pull latest postgres image
-docker pull postgres
-
-# create database container
-docker run --name borrow-my-garden-db --network borrow-my-garden-network -p 6543:5432 -v borrow-my-garden-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-
