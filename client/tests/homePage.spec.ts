fe48443c3f8403164a0c1d8322283b47182dfc63
leah-massey <15648177+leah-massey@users.noreply.github.com>
2025-04-06 20:08:16 +0100

    TEST: failing playwright e2e test written

--- client/tests/homePage.spec.ts
+++ client/tests/homePage.spec.ts
@@ -19,3 +19,21 @@ test('can navigate to a form page for adding a garden', async ({page}) => {
     await page.getByRole('link', {name: 'Add a garden'}).click();
     await expect(page.getByRole('heading', {name: 'Add a Garden'})).toBeVisible()
 })
+
+test('a new user can create a profile', async ({page}) => {
+    await page.goto('http://localhost:5173/gardens');
+    await page.getByRole('link', {name: 'Log in'}).click();
+    await expect(page.getByRole('heading', {name: 'Log in'})).toBeVisible();
+    await page.getByRole('button', {name: 'Sign up'}).click();
+    await expect(page.getByRole('heading', {name: 'Sign up'}))
+
+    await page.getByLabel('First name').fill('Maggie');
+    await page.getByLabel('Last name').fill('Brown');
+    await page.getByLabel('Email address').fill('maggie.brown@test.com');
+    await page.getByLabel('Password').fill('test123');
+    // profile image is optional
+    await page.getByLabel('Bio').fill('Looking for a garden');
+    await page.getByRole('button', {name: 'Submit'}).click();
+    await page.waitForURL('http://localhost:5173/welcome');
+    await expect(page.getByRole('heading', {name: 'Thanks for signing up, Maggie!'})).toBeVisible()
+})
